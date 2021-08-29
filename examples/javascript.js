17f135e92ceb6a7931d3195f91f8648d1b954ef9
Jannis Berndt <berndtjannis@gmail.com>
2021-08-29 15:22:04 +0200

    Adding example files to check theme coloring

---
+++ examples/javascript.js
@@ -0,0 +1,29 @@
+var globalVar;
+/**
+ * Constructor for <code>AjaxRequest</code> class
+ * @param url the url for the request<p/>
+ */
+function AjaxRequest(url) {
+  var urls = ["www.cnn.com", 5, globalVar];
+  this.request = new XMLHttpRequest();
+  url = url.replace(/^\s*(.*)/, "$1"); // skip leading whitespace
+  /* check the url to be in urls */
+  var a = "\u1111z\n\u11ac";
+  this.foo = new (function() {})();
+  foo();
+  // #
+  const cons = "abc";
+  let a = true;
+  console.log(cons)
+}
+let myObj = {
+  first: "first",
+  second: 3,
+  o: {
+    hello: "world"
+  }
+};
+class NameClass {}
+foo({ abc: "abcde" });
+foo.bar({ foo: "abc" });
+obj.abc = function() {};
